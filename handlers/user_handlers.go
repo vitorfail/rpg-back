38ff61e827132defcfa3a36b22607b0701ed20e3
vitorfail <vitor_andrademanoel@hotmail.com>
2024-11-19 21:15:25 -0300

    colocando mais funcoes

--- handlers/user_handlers.go
+++ handlers/user_handlers.go
@@ -6,10 +6,52 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
-
+	"github.com/dgrijalva/jwt-go"
 	"rpg-back/models"
+	"strings"
 )
+var jwtKey = []byte("amazonéputadecarrroabertopelosacodogildaserranarutoeshippuden")
 
+// Função para gerar o JWT
+func generateJWT(user models.User) (string, error) {
+	// Define os dados do payload
+	claims := &jwt.StandardClaims{
+		Subject:   user.Username,     // Nome do usuário ou qualquer dado relevante
+		Id:        string(user.ID),    // ID do usuário
+		Issuer:    "seu_issuer",      // Identificador da origem do token
+		ExpiresAt: time.Now().Add(24 * time.Hour).Unix(),  // Defina o tempo de expiração do token
+	}
+
+	// Criação do token com o método de assinatura HS256
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Assina o token com a chave secreta
+	signedToken, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+// Função para enviar respostas de erro em JSON
+func sendError(w http.ResponseWriter, statusCode int, message string, details string) {
+	if strings.Contains(details, "duplicate"){
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(statusCode)
+		errorResponse := map[string]string{
+			"error":   message,
+			"details": "NAME EXIST",
+		}
+		json.NewEncoder(w).Encode(errorResponse)
+	}else{
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(statusCode)
+		errorResponse := map[string]string{
+			"error":   message,
+			"details": details,
+		}
+		json.NewEncoder(w).Encode(errorResponse)
+	}
+
+}
 // CreateUser insere um novo usuário no banco de dados
 func CreateUser(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -24,15 +66,24 @@ func CreateUser(db *sql.DB) http.HandlerFunc {
 		err := db.QueryRow(sqlStatement, user.Username, user.Password).Scan(&user.ID)
 		if err != nil {
 			log.Printf("Erro ao inserir usuário no banco de dados: %v", err)
-			http.Error(w, "Erro ao criar usuário", http.StatusInternalServerError)
+			sendError(w, http.StatusInternalServerError, "Erro ao criar usuário", err.Error())
 			return
 		}
 		duration := time.Since(start)
 
 		// Imprime o tempo de execução
 		log.Printf("Tempo de execução para criar o usuário: %v", duration)
+		token, err := generateJWT(user)
+		if err != nil {
+			log.Printf("Erro ao gerar JWT: %v", err)
+			http.Error(w, "Erro ao gerar token", http.StatusInternalServerError)
+			return
+		}
+
+		// Defina o cabeçalho da resposta
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(user)
+		// Retorne o token JWT na resposta
+		json.NewEncoder(w).Encode(map[string]string{"token": token})
 	}
 }
 // CreateUser insere um novo usuário no banco de dados
